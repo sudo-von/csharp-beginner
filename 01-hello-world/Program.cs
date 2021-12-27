46ba12576a95cd0294fa2a8fe1645ef78854f46b
VoN <sudo.von.contact@gmail.com>
2021-12-27 17:40:53 -0600

    feat: :sparkles: Add hello world

---
+++ 01-hello-world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Congratulations!");
+            Console.WriteLine("You wrote your first lines of code!");
+        }
+    }
+}
