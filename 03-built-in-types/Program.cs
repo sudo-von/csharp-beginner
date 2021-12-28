b912d8d73512d5cbbc856b937eafdd7fd918ce46
VoN <sudo.von.contact@gmail.com>
2021-12-28 00:09:35 -0600

    feat: :sparkles: Add built in types

---
+++ 03-built-in-types/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03_built_in_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            string y = "VoN";
+            short z = 20;
+            Int32 xAlias = 5;
+            String yAlias = "VoN";
+            Int16 zAlias = 20;
+        }
+    }
+}
