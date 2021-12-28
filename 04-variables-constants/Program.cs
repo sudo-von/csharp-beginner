409157610a67a888bd2a1bd87e227ed48e359e93
VoN <sudo.von.contact@gmail.com>
2021-12-28 10:02:22 -0600

    feat: :sparkles: Add variables constants

---
+++ 04-variables-constants/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04_variables_constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 1;
+
+            const float PI = 3.14f;
+            byte bt = 1;
+            short st = 122;
+            int it = 245;
+            long lg = 3550;
+            float ft = 300.0f;
+            double de = 302.2;
+            decimal dl = 3000.2m;
+            char chr = 'F';
+            bool bl = true;
+        }
+    }
+}
