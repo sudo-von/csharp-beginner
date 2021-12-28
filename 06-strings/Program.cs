ad795e8274283e76da9a32abaefce71af22ef9a9
VoN <sudo.von.contact@gmail.com>
2021-12-28 10:42:21 -0600

    feat: :sparkles: Add strings

---
+++ 06-strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            if(string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String is null or empty");
+            }
+
+            string str2 = "";
+            if(string.IsNullOrWhiteSpace(str2))
+            {
+                Console.WriteLine("String is null or WhiteSpace");
+            }
+        }
+    }
+}
